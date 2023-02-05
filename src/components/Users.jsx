7ce1fdd8484449e840367d71255fc93fd5017e92
yelkarasidogancan <yelkarasidogancan@gmail.com>
2023-02-05 15:25:06 +0300

    axios

---
+++ src/components/Users.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const { data: users } = await axios(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const { data: posts } = await axios(
+        `https://jsonplaceholder.typicode.com/posts?userId=${users[0].id}`
+      );
+      setLoading(false);
+      setUsers(users);
+      setPosts(posts);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+  return (
+    <div>
+      <h2>Users</h2>
+      {loading && <div>Yükleniyor</div>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <h2>Posts</h2>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Users;
